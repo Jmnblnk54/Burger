c840e5131ba6e96fa0ee5326f6226c520b96d16e
Joshua Blank <jmnblnk@yahoo.com>
2020-09-17 18:21:04 -0700

    make it render

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
+    port: 3306,
     user: "root",
     password: "Yana5624!", //password goes here
     database: "burgers_DB"
